8a5c1d4d7c43c5e0963a7b028d8607fd54c4999c
Penny <26400127+pennych@users.noreply.github.com>
2023-04-17 14:50:38 -0600

    Removed unused imports and formatted code.

--- src/panels/SpeciesForm.java
+++ src/panels/SpeciesForm.java
@@ -1,32 +1,23 @@
 package panels;
 
-import java.awt.Color;
-import java.awt.Font;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-
 import sql.SqlESAConservationStatus;
 import sql.SqlRegion;
 import sql.SqlThreat;
-
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JTextField;
-import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
-import javax.swing.JList;
-import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.awt.event.ActionEvent;
 
 /**
- * Takes in user input to create a new species entry to the Endangered Species table
+ * Takes in user input to create a new species entry to the Endangered Species
+ * table
  * 
  * @author Penny Chanthavong, et al.
  */
@@ -46,40 +37,39 @@ public class SpeciesForm extends JPanel {
 	public SpeciesForm() {
 		setLayout(null);
 		setBounds(100, 100, 1100, 900);
-		
-		
+
 		JLabel InstructionLabel = new JLabel("Add a new species to track conservation efforts: ");
 		InstructionLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		InstructionLabel.setBounds(6, 314, 1088, 16);
 		add(InstructionLabel);
-		
-		// = = =  NAME = = =
+
+		// = = = NAME = = =
 		NameField = new JTextField();
-		NameField.setBounds(182, 375, 206, 47);
+		NameField.setBounds(145, 375, 206, 47);
 		add(NameField);
 		NameField.setColumns(10);
-		
+
 		JLabel CommonNameLabel = new JLabel("Common Name: ");
 		CommonNameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		CommonNameLabel.setBounds(61, 375, 109, 47);
+		CommonNameLabel.setBounds(41, 375, 94, 47);
 		add(CommonNameLabel);
-		
+
 		JLabel ScientificNameLabel = new JLabel("Scientific Name: ");
 		ScientificNameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		ScientificNameLabel.setBounds(61, 454, 109, 47);
+		ScientificNameLabel.setBounds(27, 454, 108, 47);
 		add(ScientificNameLabel);
-		
+
 		ScientificField = new JTextField();
 		ScientificField.setColumns(10);
-		ScientificField.setBounds(182, 454, 206, 47);
+		ScientificField.setBounds(145, 454, 206, 47);
 		add(ScientificField);
-		
-		// = = = CONSERVATION STATUS = = =	
+
+		// = = = CONSERVATION STATUS = = =
 		JLabel ESALabel = new JLabel("ESA Status:");
 		ESALabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		ESALabel.setBounds(661, 526, 134, 47);
+		ESALabel.setBounds(681, 527, 73, 47);
 		add(ESALabel);
-		
+
 		/*
 		 * @author Penny Chanthavong
 		 */
@@ -87,26 +77,25 @@ public class SpeciesForm extends JPanel {
 		try (Connection connection = DriverManager.getConnection("jdbc:derby:EndangeredSpeciesDB;create=true");
 				Statement statement = connection.createStatement();) {
 			ResultSet results = statement.executeQuery(SqlESAConservationStatus.getStatus());
-			while(results.next()) {
+			while (results.next()) {
 				String name = results.getString("Status");
 				ESASelection.addItem(name);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		ESASelection.setBounds(800, 537, 238, 27);
+		ESASelection.setBounds(764, 537, 238, 27);
 		add(ESASelection);
-		
+
 		// = = = REGION = = =
 		JLabel RegionLabel = new JLabel("Region:");
 		RegionLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		RegionLabel.setBounds(711, 454, 84, 47);
+		RegionLabel.setBounds(689, 454, 65, 47);
 		add(RegionLabel);
-		
-		String[] regions = {"West Desert", "Green River", "Color Country", 
-				"Paria River", "Canyon Country"};
-		
+
+		String[] regions = { "West Desert", "Green River", "Color Country", "Paria River", "Canyon Country" };
+
 		/*
 		 * @author Penny Chanthavong
 		 */
@@ -114,23 +103,23 @@ public class SpeciesForm extends JPanel {
 		try (Connection connection = DriverManager.getConnection("jdbc:derby:EndangeredSpeciesDB;create=true");
 				Statement statement = connection.createStatement();) {
 			ResultSet results = statement.executeQuery(SqlRegion.getDistrictFromForm());
-			while(results.next()) {
+			while (results.next()) {
 				String name = results.getString("District");
 				RegionSelection.addItem(name);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		RegionSelection.setBounds(800, 465, 238, 27);
+		RegionSelection.setBounds(764, 464, 238, 27);
 		add(RegionSelection);
-		
+
 		// = = = THREATS = = =
 		JLabel ThreatsLabel = new JLabel("Threats: ");
 		ThreatsLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		ThreatsLabel.setBounds(701, 375, 98, 47);
+		ThreatsLabel.setBounds(681, 375, 73, 47);
 		add(ThreatsLabel);
-		
+
 		/*
 		 * @author Penny Chanthavong
 		 */
@@ -138,47 +127,45 @@ public class SpeciesForm extends JPanel {
 		try (Connection connection = DriverManager.getConnection("jdbc:derby:EndangeredSpeciesDB;create=true");
 				Statement statement = connection.createStatement();) {
 			ResultSet results = statement.executeQuery(SqlThreat.getDescriptionFromForm());
-			while(results.next()) {
+			while (results.next()) {
 				String name = results.getString("Description");
 				ThreatsSelection.addItem(name);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		ThreatsSelection.setBounds(800, 386, 238, 27);
+
+		ThreatsSelection.setBounds(764, 385, 238, 27);
 		add(ThreatsSelection);
-		
+
 		// = = = POPULATION = = =
 		JLabel PopulationLabel = new JLabel("Population: ");
 		PopulationLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		PopulationLabel.setBounds(384, 375, 98, 47);
+		PopulationLabel.setBounds(380, 375, 73, 47);
 		add(PopulationLabel);
-		
+
 		PopulationField = new JTextField();
 		PopulationField.setColumns(10);
-		PopulationField.setBounds(490, 375, 206, 47);
+		PopulationField.setBounds(463, 375, 206, 47);
 		add(PopulationField);
-		
+
 		// = = = CLASS = = =
 		JLabel ClassLabel = new JLabel("Class:");
 		ClassLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		ClassLabel.setBounds(413, 454, 65, 47);
+		ClassLabel.setBounds(388, 454, 65, 47);
 		add(ClassLabel);
-		
+
 		ClassField = new JTextField();
 		ClassField.setColumns(10);
-		ClassField.setBounds(490, 454, 206, 47);
+		ClassField.setBounds(463, 454, 206, 47);
 		add(ClassField);
-		
+
 		// = = = SUBMIT = = =
 		SubmitBtn = new JButton("SUBMIT");
-		SubmitBtn.setBounds(60, 695, 977, 90);
+		SubmitBtn.setBounds(447, 642, 206, 37);
 		add(SubmitBtn);
 	}
-
-
 
 	/**
 	 * @return the nameField
@@ -193,26 +180,26 @@ public class SpeciesForm extends JPanel {
 	public String getScientificField() {
 		return ScientificField.getText();
 	}
-	
+
 	/**
 	 * @return the classField
 	 */
 	public String getClassField() {
 		return ClassField.getText();
 	}
-	
+
 	/**
 	 * @return the populationField
 	 */
 	public int getPopulationField() {
 		return Integer.parseInt(PopulationField.getText());
 	}
-	
+
 	public int getThreatSelection() {
 		int index = ThreatsSelection.getSelectedIndex() + 1;
 		return index;
 	}
-	
+
 	/**
 	 * @return the regionSelection
 	 */
@@ -220,13 +207,13 @@ public class SpeciesForm extends JPanel {
 		int index = RegionSelection.getSelectedIndex() + 1;
 		return index;
 	}
-	
+
 	/**
 	 * @return the eSASelection
 	 */
 	public int getESASelection() {
 		int index = ESASelection.getSelectedIndex() + 123;
-		return index;	
+		return index;
 	}
 
 	public JButton getSubmitBtn() {
